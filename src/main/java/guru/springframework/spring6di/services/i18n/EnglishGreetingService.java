6d0f9465786085c8876816b6f523be2b2619a264
Yazdan <1yazdanrafieemanesh@gmail.com>
2025-02-15 22:53:09 +0330

    Feature: test active profile

---
+++ src/main/java/guru/springframework/spring6di/services/i18n/EnglishGreetingService.java
@@ -0,0 +1,14 @@
+package guru.springframework.spring6di.services.i18n;
+
+import guru.springframework.spring6di.services.GreetingService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Profile("EN")
+@Service("i18NService")
+public class EnglishGreetingService implements GreetingService {
+    @Override
+    public String sayGreeting() {
+        return "Hello World - EN";
+    }
+}
