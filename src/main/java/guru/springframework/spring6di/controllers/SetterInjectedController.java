f4556d5ee18e07bee841f34231f4fbca937fee04
Yazdan <1yazdanrafieemanesh@gmail.com>
2025-02-15 21:59:24 +0330

    Feature: test qualifier annotation

--- src/main/java/guru/springframework/spring6di/controllers/SetterInjectedController.java
+++ src/main/java/guru/springframework/spring6di/controllers/SetterInjectedController.java
@@ -2,6 +2,7 @@ package guru.springframework.spring6di.controllers;
 
 import guru.springframework.spring6di.services.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -9,8 +10,9 @@ public class SetterInjectedController {
 
     private GreetingService greetingService;
 
+
     @Autowired
-    public void setGreetingService(GreetingService greetingService) {
+    public void setGreetingService(@Qualifier("setterGreetingBean") GreetingService greetingService) {
         this.greetingService = greetingService;
     }
 
