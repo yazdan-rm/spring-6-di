598880e5939a28bd6ac609340ad9eb1753082719
Yazdan <1yazdanrafieemanesh@gmail.com>
2025-02-15 23:10:34 +0330

    Feature: test default profile

--- src/main/java/guru/springframework/spring6di/services/i18n/EnglishGreetingService.java
+++ src/main/java/guru/springframework/spring6di/services/i18n/EnglishGreetingService.java
@@ -4,7 +4,7 @@ import guru.springframework.spring6di.services.GreetingService;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("EN")
+@Profile({"EN", "default"})
 @Service("i18NService")
 public class EnglishGreetingService implements GreetingService {
     @Override
