d22f4be0b3fff20728d996418d0985dda1dbd5d3
Yazdan <1yazdanrafieemanesh@gmail.com>
2025-02-15 19:06:33 +0330

    Feature: add some test of application context

--- src/test/java/guru/springframework/spring6di/Spring6DiApplicationTests.java
+++ src/test/java/guru/springframework/spring6di/Spring6DiApplicationTests.java
@@ -1,10 +1,30 @@
 package guru.springframework.spring6di;
 
+import guru.springframework.spring6di.controllers.MyController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
-class Spring6DiApplicationTests {
+class  Spring6DiApplicationTests {
+
+    @Autowired
+    ApplicationContext applicationContext;
+
+    @Autowired
+    MyController controller;
+
+    @Test
+    void testAutowireOfController() {
+        System.out.println(controller.sayHello());
+    }
+
+    @Test
+    void testGetControllerFromCtx(){
+        MyController controller = applicationContext.getBean(MyController.class);
+        System.out.println(controller.sayHello());
+    }
 
     @Test
     void contextLoads() {
