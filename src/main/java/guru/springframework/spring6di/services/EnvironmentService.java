9f59ea895acdb7d80698b83136f7c5860d9d6892
Yazdan <1yazdanrafieemanesh@gmail.com>
2025-02-16 01:49:57 +0330

    Feature: solve assignment

---
+++ src/main/java/guru/springframework/spring6di/services/EnvironmentService.java
@@ -0,0 +1,6 @@
+package guru.springframework.spring6di.services;
+
+public interface EnvironmentService {
+
+    String getEnv();
+}
---
+++ src/test/java/guru/springframework/spring6di/controllers/EnvironmentControllerTest.java
@@ -0,0 +1,21 @@
+package guru.springframework.spring6di.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.*;
+@ActiveProfiles({"prod", "EN"})
+@SpringBootTest
+class EnvironmentControllerTest {
+
+    @Autowired
+    EnvironmentController environmentController;
+
+    @Test
+    void getEnvironment() {
+        System.out.println(environmentController.getEnvironment());
+    }
+}
