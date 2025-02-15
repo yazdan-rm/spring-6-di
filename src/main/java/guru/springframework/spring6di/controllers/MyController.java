69d784b3b9e23928d9b27ea436d3245a7892c7e6
Yazdan <1yazdanrafieemanesh@gmail.com>
2025-02-15 19:45:15 +0330

    Feature: test without di

--- src/main/java/guru/springframework/spring6di/controllers/MyController.java
+++ src/main/java/guru/springframework/spring6di/controllers/MyController.java
@@ -1,12 +1,20 @@
 package guru.springframework.spring6di.controllers;
 
+import guru.springframework.spring6di.services.GreetingService;
+import guru.springframework.spring6di.services.GreetingServiceImpl;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
 
+    private final GreetingService greetingService;
+
+    public MyController() {
+        this.greetingService = new GreetingServiceImpl();
+    }
+
     public String sayHello() {
         System.out.println("I'm in the controller");
-        return "Hello Everyone!!!!!";
+        return greetingService.sayGreeting();
     }
 }
---
+++ src/main/java/guru/springframework/spring6di/services/GreetingServiceImpl.java
@@ -0,0 +1,9 @@
+package guru.springframework.spring6di.services;
+
+public class GreetingServiceImpl implements GreetingService {
+
+    @Override
+    public String sayGreeting() {
+        return "Hello Everyone From Base Service!";
+    }
+}
