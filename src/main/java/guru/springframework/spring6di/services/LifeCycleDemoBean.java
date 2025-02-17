31af3e397bbed3f188727730e73a2e77b0778c0f
Yazdan <1yazdanrafieemanesh@gmail.com>
2025-02-18 01:01:59 +0330

    Feature: solve assignment

--- src/main/java/guru/springframework/spring6di/services/LifeCycleDemoBean.java
+++ src/main/java/guru/springframework/spring6di/services/LifeCycleDemoBean.java
@@ -67,8 +67,7 @@ public class LifeCycleDemoBean implements InitializingBean, DisposableBean, Bean
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
         System.out.println("## postProcessBeforeInitialization: " + beanName);
 
-        if (bean instanceof MyController){
-            MyController myController = (MyController) bean;
+        if (bean instanceof MyController myController){
             System.out.println("Calling before init");
             myController.beforeInit();
         }
